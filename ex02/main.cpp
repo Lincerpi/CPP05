efc765daaec9c29b1e85a6873eb6092348a538b0
lince <lincerpi@student.42roma.it>
2021-07-12 14:07:47 +0200

    C++05

---
+++ ex02/main.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+
+int main()
+{
+	
+	
+	return (0);
+}
---
+++ ex03/main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "Intern.hpp"
+
+int main()
+{
+	
+	
+	return (0);
+}
