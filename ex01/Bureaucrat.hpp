d533cf4b223a6a33916640ca20dfb02f4058bcb6
lince <lincerpi@student.42roma.it>
2021-07-12 13:23:20 +0200

    C++05

---
+++ ex01/Bureaucrat.hpp
@@ -0,0 +1,34 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+
+class Bureaucrat
+{
+		std::string const name;
+		int	grade;
+		Bureaucrat();
+
+	public:
+		Bureaucrat(std::string const &name, int grade);
+		virtual ~Bureaucrat();
+		Bureaucrat(const Bureaucrat& copy);
+		Bureaucrat& operator=(const Bureaucrat& op);
+		std::string const &getName() const;
+		int getGrade() const;
+		class GradeTooHighException: public std::exception
+		{
+			virtual const char* what() const throw();
+		};
+		class GradeTooLowException: public std::exception
+		{
+			virtual const char* what() const throw();
+		};
+		void incrementGrade();
+		void decrementGrade();
+};
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& bureaucrat);
+
+#endif
---
+++ ex01/main.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int main()
+{
+	
+	
+	return (0);
+}
